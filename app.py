13172a0bfb89a1343d397f5a9a3a5e836f3fc0db
kei620809 <kei620809@gmail.com>
2025-08-15 18:25:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
 
 # データベースの接続設定。Renderの環境変数から接続URLを取得します。
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://scheduler_db_hcsm_user:5UI1QubE1yrMYTgAdZpdtzODQaekDfBy@dpg-d2ffjn6r433s73b9r84g-a/scheduler_db_hcsm')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # データベースオブジェクトの初期化
@@ -23,4 +23,5 @@ def index():
 
 # --- 実行 ---
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=8080, debug=True)
